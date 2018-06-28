8d09b35f89be8c421d916d4f51bcb4955ac1f8d9
shray <shray@goswami.info>
2018-06-28 23:04:21 +0530

    DONE ;)

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,29 +1,86 @@
 package com.londonappbrewery.destini;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
-
+    TextView ques;
+    Button o1,o2;
+    int Index = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ques = findViewById(R.id.storyTextView);
+        o1=findViewById(R.id.buttonTop);
+        o2=findViewById(R.id.buttonBottom);
+        ques.setText(R.string.T1_Story);
+        o1.setText(R.string.T1_Ans1);
+        o2.setText(R.string.T1_Ans2);
+        o1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+                if(Index==1)
+                {
+                    ques.setText(R.string.T3_Story);
+                    o1.setText(R.string.T3_Ans1);;
+                    o2.setText(R.string.T3_Ans2);
+                    Index=3;
+                }
+               else if(Index==3)
+                {
+                    ques.setText(R.string.T6_End);
+                    o1.setText("");
+                    o2.setText("");
+                    Index=0;
+                }
+                else
+                {
+                    ques.setText(R.string.T3_Story);
+                    o1.setText(R.string.T3_Ans1);
+                    o2.setText(R.string.T3_Ans2);
+                    Index=3;
+                }
 
+            }
+        });
+        o2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if(Index==1)
+                {
+                    ques.setText(R.string.T2_Story);
+                    o1.setText(R.string.T2_Ans1);
+                    o2.setText(R.string.T2_Ans2);
+                    Index=2;
+                }
+                else if(Index==2)
+                {
+                    ques.setText(R.string.T4_End);
+                    o1.setText("");
+                    o2.setText("");
+                    Index=0;
+                }
+                else
+                {
+                    ques.setText(R.string.T5_End);
+                    o1.setText("");
+                    o2.setText("");
+                    Index=0;
+                }
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
-
-
-
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+            }
+        });
 
 
     }
